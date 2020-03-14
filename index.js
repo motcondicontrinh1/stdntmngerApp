0e42e407c0f9bc0d74b6805c7e25f02e9ce22400
Dương Bảo Hưng <somac@MacBook-Pro-cua-Duong.local>
2020-03-14 12:08:04 +0700

    Add function deleteAllStudents

--- index.js
+++ index.js
@@ -3,62 +3,78 @@
 // add new student
 // flow chart: hoat dong cua capp
 
-let readlineSync = require('readline-sync');
-let fs = require('fs');
+let readlineSync = require("readline-sync");
+let fs = require("fs");
 
 let students = [];
+
+const deleteAllStudents = () => {
+    students.splice(1, students.length - 1);
+    const content = JSON.stringify(students);
+    fs.writeFileSync("./data.json", content, {
+        econding: "utf8"
+    });
+}
+
 const showMenu = () => {
     console.log(`1. Show all students`);
     console.log(`2. Create a new student`);
     console.log(`3. Save and exit`);
-    const option = readlineSync.question('> ');
+    console.log(`4. Delete all students`);
+    const option = readlineSync.question("> ");
     console.log(option);
     switch (option) {
-        case '1':
-            showStudents();    
-            showMenu(); 
+        case "1":
+            showStudents();
+            showMenu();
             break;
-        case '2':
+        case "2":
             showCreateStudent();
-            showMenu();   
+            showMenu();
             break;
-        case '3':
-            saveAndExit();     
+        case "3":
+            saveAndExit();
+            break;
+        case "4":
+            deleteAllStudents();
+            showMenu();
             break;
         default:
             console.log(`Wrong option.`);
             showMenu();
             break;
-    };
-}
+    }
+};
 
 const loadData = () => {
-    let fileContent = fs.readFileSync('./data.json');
+    let fileContent = fs.readFileSync("./data.json");
     students = JSON.parse(fileContent);
-}
+};
 
 const showStudents = () => {
     students.forEach(cur => console.log(cur.name, cur.age));
-}
+};
 
-const showCreateStudent = () =>{
-    var name = readlineSync.question('Name: ');
-    var age = readlineSync.question('age: ');
+const showCreateStudent = () => {
+    var name = readlineSync.question("Name: ");
+    var age = readlineSync.question("age: ");
     var student = {
         name: name,
         age: parseInt(age)
-    }
-    students.push(student); 
-}
+    };
+    students.push(student);
+};
 
 const saveAndExit = () => {
     var content = JSON.stringify(students);
-    fs.writeFileSync('./data.json', content, {econding: 'utf8'});
+    fs.writeFileSync("./data.json", content, {
+        econding: "utf8"
+    });
 };
 
 const main = () => {
     loadData();
     showMenu();
-}
+};
 
 main();
